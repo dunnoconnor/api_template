d8fccf03666be841ffe4b3c48febcec994bcf3f0
Newman <newmer8@ad.vzwcorp.com>
2022-04-04 18:49:31 -0500

    seeded database and add route table in app.js

--- app.js
+++ app.js
@@ -20,6 +20,74 @@ app.get('/', (req, res) => {
   res.send('<h1>App Running</h1>')
 })
 
+// returns all items
+app.get('/items', async(req, res) =>{
+    let items = await Item.findAll();
+    res.json({items});
+  })
+// returns one item by id
+app.get(`/items/:id`, async (req,res) => {
+    const singleitems = await Item.findByPk(req.params.id);
+    res.json({singleitems});
+  })
+
+//return one item by name
+// app.get('/item-name/:name', async(req,res)=>{   
+//   const thisItem = await Item.findOne({where:{name: req.params.name}})
+//   res.json(thisItem)
+
+// returns all users
+app.get('/users', async(req, res) =>{
+    let users = await User.findAll();
+    res.json({users});
+  })
+// returns one user by id
+  app.get(`/users/:id`, async (req,res) => {
+    const singleusers = await User.findByPk(req.params.id);
+    res.json({singleusers});
+  })
+// creates one new item
+app.post('/items', async(req,res) =>{
+  let newItem = await Item.create(req.body);
+  res.json({newItem})
+})
+// creates one new user
+app.post('/users', async(req,res) =>{
+  let newUser = await User.create(req.body);
+  res.json({newUser})
+})
+//update one item by id
+app.put('/items/:id', async (req,res) => {
+  let updatedItem = await Item.update(req.body, {
+      where : {id: req.params.id}
+  })
+  res.send(updatedItem ? "Item Updated" : "update Failed")
+})
+//update one user by id
+app.put('/users/:id', async (req,res) => {
+  let updatedUser = await User.update(req.body, {
+      where : {id: req.params.id}
+  })
+  res.send(updatedUser ? "User Updated" : "update Failed")
+})
+
+//DELETE method, items/:id path => Deletes an item from db.sqlite
+app.delete("/items/:id", async (req, res) => {
+  const deletedItem = await Item.destroy({
+    where: { id: req.params.id },
+  });
+  res.send(deletedItem ? "Deleted" : "Deletion Failed");
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const deletedUser = await User.destroy({
+    where: { id: req.params.id },
+  });
+  res.send(deletedUser ? "Deleted" : "Deletion Failed");
+});
+
+
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
